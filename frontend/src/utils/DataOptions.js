1de3486ace3c969b584258f020ab9aecbc010f57
JohnnySimple <obengboatengjohnson@gmail.com>
2022-06-25 23:21:17 +0000

    add some utility functions

---
+++ frontend/src/utils/DataOptions.js
@@ -0,0 +1,14 @@
+export const get_data_options = () => {
+    return {
+        plugins: {
+            title: {
+                display: true,
+                text: "TestingChart"
+            },
+            legend: {
+                display: true,
+                position: "bottom"
+            }
+        }
+    }
+}
---
+++ frontend/src/utils/commented.js
@@ -0,0 +1,30 @@
+// labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+// data = {
+//     labels,
+//     datasets: [
+//         {
+//             label: "Dataset 1",
+//             data: [50, 55, 60, 65, 70, 75, 80],
+//             backgroundColor: "rgba(255, 99, 132, 0.5)"
+//         },
+//         {
+//             label: "Dataset 2",
+//             data: [50, 55, 60, 65, 70, 75, 80],
+//             backgroundColor: "rgba(53, 162, 235, 0.5)"
+//         }
+//     ],
+// };
+
+// options = {
+//     plugins: {
+//         title: {
+//             display: true,
+//             text: "TestingChart"
+//         },
+//         legend: {
+//             display: true,
+//             position: "bottom"
+//         }
+//     }
+// }
